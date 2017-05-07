d2b031d16f1de44ab045501da86701e003e27d43
o0YeHuang <std.ye.huang@gmail.com>
2017-05-06 23:23:35 -0700

    Added app/static/css/style.css; Modified config.py to support localhost database; freeze requirements.txt

--- app/config.py
+++ app/config.py
@@ -13,16 +13,16 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_ECHO = True
     SECRET_KEY = THE_SECRET_KEY
-    DB_HOST = os.getenv('IP', '0.0.0.0')
-    SQLALCHEMY_DATABASE_URI = "mysql://" + DB_USER + ":" + DB_PASS + "@" + DB_HOST + "/" + DB_NAME
+    #DB_HOST = os.getenv('IP', 'localhost')
+    SQLALCHEMY_DATABASE_URI = "postgresql://ye_huang:@localhost:5432/bluehat"
 
 # Production configurations
 class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_ECHO = True
     SECRET_KEY = THE_SECRET_KEY
-    DB_HOST = os.getenv('IP', '0.0.0.0')
-    SQLALCHEMY_DATABASE_URI = "mysql://" + DB_USER + ":" + DB_PASS + "@" + DB_HOST + "/" + DB_NAME
+    #DB_HOST = os.getenv('IP', '0.0.0.0')
+    SQLALCHEMY_DATABASE_URI = "postgresql://ye_huang:@localhost:5432/bluehat"
 
 app_config = {
     'development': DevelopmentConfig,
