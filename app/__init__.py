33fca1943359288f36419ebb4c50aa7926646d39
Moiz Qureshi <moizq@calpak-usa.com>
2017-05-13 20:10:00 -0700

    Fixed bluehat flask app structure

--- app/__init__.py
+++ app/__init__.py
@@ -13,25 +13,24 @@ from env import *
 # db variable initialization
 db = SQLAlchemy()
 
-# create the actual Flask app and our associated db objects based on a given config_name
-def create_app(config_name) :
-    # App Config
-    app = Flask(__name__)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    app.config['SQLALCHEMY_ECHO'] = False
+# App Config
+app = Flask(__name__)
+app.config.from_object(app_config[FLASK_CONFIG])
+app.config.from_pyfile('config.py')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_ECHO'] = False
 
-    # DB Config
-    db.init_app(app)
+# DB Config
+db.init_app(app)
 
-    # Migration Config
-    migration = Migrate(app, db)
+# Migration Config
+migration = Migrate(app, db)
 
-    # Session Config
-    app.secret_key = THE_SECRET_KEY
-    app.config['SESSION_TYPE'] = 'filesystem'
-    Session(app)
+# Session Config
+app.secret_key = THE_SECRET_KEY
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
-    # Finally return our app object
-    return app
+
+# Late import so modules can import their dependencies properly
+from app import models, views
