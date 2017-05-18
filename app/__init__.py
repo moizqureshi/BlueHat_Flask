4357def599c44a06e02c9dd681630e886d016855
Moiz Qureshi <moizq@calpak-usa.com>
2017-05-18 13:04:32 -0700

    Configured flask gzip compress, AWS CDN for heroku site, and also flask login

--- app/__init__.py
+++ app/__init__.py
@@ -8,21 +8,35 @@ from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
+from flask_login import LoginManager
+from flask_compress import Compress
+from flask_cdn import CDN
 
+# Socket Init Setting
+async_mode = "eventlet"
 
 # db variable initialization
 db = SQLAlchemy()
 
-# Basic App Config
+# flask-login initialization
+login_manager = LoginManager()
+
+# cdn initialization
+cdn = CDN()
+
+# compress initialization
+compress = Compress()
+
+# App Config
 app = Flask(__name__)
-
-# SocketIO Config
-socketio = SocketIO(app)
-
+socketio = SocketIO(app, async_mode=async_mode)
 app.config.from_object(app_config[FLASK_CONFIG])
 app.config.from_pyfile('config.py')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_ECHO'] = False
+app.config['CDN_DOMAIN'] = 'd30bzjua38r0w8.cloudfront.net'
+app.config['CDN_HTTPS'] = True
+app.config['CDN_DEBUG'] = True
 
 # DB Config
 db.init_app(app)
@@ -34,6 +48,15 @@ migration = Migrate(app, db)
 app.secret_key = THE_SECRET_KEY
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
+
+# CDN Config
+cdn.init_app(app)
+
+# flask-compress config
+compress.init_app(app)
+
+# flask-login config
+login_manager.init_app(app)
 
 
 # Late import so modules can import their dependencies properly
