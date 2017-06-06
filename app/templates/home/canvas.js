d46745c9b23c364144bbf7cb01b8a8ce5c1446cb
Godric <ben.aymane@gmail.com>
2017-06-06 14:24:53 -0700

    Added the mapping functionality and wrote our own library for it

---
+++ app/templates/home/canvas.js
@@ -0,0 +1,102 @@
+//Custom Canvas Object to hold all we need and make it easy
+function Canvas(tag) {
+  this.drawable = tag.getContext("2d"); //holds the canvas
+  this.init = init;
+  this.addMarkerXY = addMarkerXY;
+  this.addMarkerInner = addMarkerInner;
+  this.clearMarkerXY = clearMarkerXY;
+  this.clearMarker = clearMarker;
+  this.drawMap = drawMap;
+  this.addMarkerList = addMarkerList;
+  this.addMarkerCoor = addMarkerCoor
+  this.setColor = setColor;
+  this.color = "#000000"; //holds the color to draw with
+  this.x = 0; //holds the last X coordinate
+  this.y = 0; //holds the last Y coordinate
+}
+
+/*
+ * Takes in a list of coordinates like [[x1,y1], [x2,y2]]
+ * and draw a polygon by iterating through the coordinates
+ * drawing points and lines between the points
+ */
+function drawMap(map) {
+  this.drawable.strokeStyle = '#000000';
+  for( i = 0; i < map.length; i++) {
+    var x = map[i][0];
+    var y = map[i][1];
+
+    if( i > 0)
+      this.drawable.lineTo(x, y)
+
+    this.addMarkerXY(x, y);
+    this.drawable.moveTo(x, y);
+  }
+  this.drawable.lineTo(map[0][0], map[0][1]);
+  this.drawable.stroke();
+}
+
+//atm this is unused.
+function init(tag) {
+  this.drawable = tag.getContext("2d");
+}
+
+/*
+ * Adds a marker at position (x,y) and saves x and y
+ * internally a.k.a in this.x and this.y
+ */
+function addMarkerXY(x, y) {
+  console.log("x = " + x + " y = " + y + " color = " + this.color);
+  this.x = x;
+  this.y = y;
+  this.drawable.strokeStyle = this.color;
+  this.drawable.strokeRect(x,y,1,1);
+}
+
+/*
+ * Adds a marker at position (this.x, this.y)
+ */
+function addMarkerInner( ) {
+  this.addMarkerXY(this.x, this.y);
+}
+
+/*
+ * Adds a lot of markers from a list of coordinates
+ * this is different from drawMap because it doesn't draw
+ * any lines.
+ */
+function addMarkerList(coordinateList) {
+  for( i = 0; i < coordinateList.length; i++)
+    this.addMarkerXY(coordinateList[i][0], coordinateList[i][1]);
+}
+
+/*
+ * This functions received a single list of 2 entries
+ * and makes a marker off of the 0th and 1st where [0] is x
+ * and [1] is y
+ */
+function addMarkerCoor(coordinates) {
+  this.addMarkerXY(coordinates[0], coordinates[1]);
+}
+
+/*
+ * removes a marker at position x, y
+ */
+function clearMarkerXY(x, y) {
+  //causes no error if x or y is 0
+  this.drawable.clearRect(x-1,y-1,3,3);
+}
+
+/*
+ * remove the marker at position (this.x, this.y)
+ */
+function clearMarker() {
+  this.clearMarkerXY(this.x, this.y);
+}
+
+/*
+ * sets the color to draw with
+ */
+function setColor(color) {
+  this.color = color;
+}
---
+++ app/templates/home/mappingFunctions.js
@@ -0,0 +1,37 @@
+//Holds a dictionary of: (peripheral person name, the peripheral's own Custom Canvas object)
+var peripherals = new Map();
+
+/*
+ * Checks if we have a peripheral Canvas object associated with a peripheral name
+ * passed in. Send true/false accordingly
+ */
+function isPeriNew(periName) {
+  //Checks in the map with the key
+  if (peripherals.get(periName) === undefined){
+    return true;
+  }
+  return false;
+}
+
+/*
+ * Makes a new Canvas object, draw a mark at (x, y)
+ * in the Canvas and then add this new object to the map
+ * and associate it with the peripheral name
+ */
+function addPeri(periName, periCanvasId, x, y) {
+  var periCanvas = new Canvas(document.getElementById(periCanvasId));
+  periCanvas.addMarkerXY(x, y);
+  peripherals.set(periName, periCanvas);
+}
+
+/*
+ * This simply gets a Canvas object from the dictionary
+ * with the peripheral person name as the key then
+ * remove the existing marker and create a new one
+ * at (x, y)
+ */
+function updatePeri(periName, x, y) {
+  var periCanvas = peripherals.get(periName);
+  periCanvas.clearMarker();
+  periCanvas.addMarkerXY(x, y);
+}
